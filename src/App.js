9a417bec3c8ef1c867587a3c74a4d301afde4aa5
Emmanuel Moran <emmanuel.moran@uabc.edu.mx>
2018-10-02 14:42:22 -0700

    Added input and LoginForm components, also, the fields email and password

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from'react-native';
+import { View } from'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCcLmlNKTCehAYCqUtId5yPmh3w2MVSQaM',
+            authDomain: 'authreactnative-75113.firebaseapp.com',
+            databaseURL: 'https://authreactnative-75113.firebaseio.com',
+            projectId: 'authreactnative-75113',
+            storageBucket: 'authreactnative-75113.appspot.com',
+            messagingSenderId: '93985325380'
+          });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm />
             </View>
         );
     }
